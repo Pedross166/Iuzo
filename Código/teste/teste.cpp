56a1da631b6ba044e564605417400f9809f97892
Pedross166 <164056610+Pedross166@users.noreply.github.com>
2026-05-16 23:22:41 -0300

    Add files via upload

---
+++ Código/teste/teste.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Hello human!" << std::endl;
+    int x;
+    std::cin >> x;
+    std::cout << "o valor de x :"<< x << std::endl;
+
+    return 0;
+}
+
